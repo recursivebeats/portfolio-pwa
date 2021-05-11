8acef7de42394ed9ec894e2e726f00abac71cbfb
robbie <robbie@robbiehall.co.uk>
2021-05-11 18:48:17 +0100

    Initial commit, strip Quasar defaults

---
+++ pwa-ui/src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+    </q-header>
+    <q-drawer>
+    </q-drawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout'
+}
+</script>
