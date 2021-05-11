fba0014ef4943a785ac93563a362a9b58bf5e536
robbie <robbie@robbiehall.co.uk>
2021-05-11 19:53:56 +0100

    Add change to test PWA updating

--- pwa-ui/src/pages/Index.vue
+++ pwa-ui/src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center q-pa-sm">
-    <div class="text-h1 text-center">This is my PWA mkVI</div>
+    <div class="text-h1 text-center">This is my PWA</div>
   </q-page>
 </template>
 
