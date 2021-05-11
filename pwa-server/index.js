3fc3bb1162f4c7616b5d64b7cc1b0bfd353df86c
robbie <robbie@robbiehall.co.uk>
2021-05-11 19:50:09 +0100

    Add change to test PWA updating

--- pwa-server/index.js
+++ pwa-server/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const path = require('path')
+const nocache = require('nocache')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.static('public'))
+app.use(nocache())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
